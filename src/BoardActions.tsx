23414c01997200359a3acd832de5ec6a6e269181
YimSungPhyo <78424764+YimSungPhyo@users.noreply.github.com>
2025-11-10 14:38:36 +0000

    hehe1

--- src/BoardActions.tsx
+++ src/BoardActions.tsx
@@ -4,7 +4,7 @@ const BoardActions = () => {
 
     return (
         <div className="board-actions">
-            <button className="write-button">글쓰기</button>
+            <button className="write-button">글쓰기2</button>
             <input type="text" placeholder="검색어를 입력하세요"/>
             <button className="search-button">검색</button>
         </div>
