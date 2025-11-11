bd54c34acff08f24638a02f386369e353555e438
YimJunHyuk <sungs6031@naver.com>
2025-11-11 23:53:48 +0900

    자식 PostList 에게 props넘기기

--- src/BoardContainer.tsx
+++ src/BoardContainer.tsx
@@ -4,6 +4,15 @@ import PostList from "./PostList";
 import PageNation from "./PageNation";
 import BoardActions from "./BoardActions"
 
+
+// 목업 데이터 (실제로는 API 호출 또는 Context/Redux에서 가져옴)
+const MOCK_POSTS = [
+    { id: 3, title: "React Mockup 테스트 게시글입니다.", author: "홍길동", date: "2025-11-10" },
+    { id: 2, title: "간단한 HTML 구조 확인", author: "김철수", date: "2025-11-09" },
+    { id: 1, title: "안녕하세요! 첫 게시글입니다.", author: "이영희", date: "2025-11-08" },
+];
+
+
 //component 
 const BoardContainer: React.FC = () => {
 
@@ -13,7 +22,7 @@ const BoardContainer: React.FC = () => {
             <h2 className="board-title">게시판</h2>
         
             {/*  component: PostList */}
-            <PostList/>
+            <PostList posts={MOCK_POSTS}/>
             
 
             {/*  component: Pagination */}
--- src/PostList.tsx
+++ src/PostList.tsx
@@ -1,9 +1,36 @@
 import React, {useState} from 'react';
 
+//{ id: 3, title: "React Mockup 테스트 게시글입니다.", author: "홍길동", date: "2025-11-10" },
+
+
+//type을 사용한경우와 interface를 사용한 경우 모두 가능
+/*  
+type post = {
+    id: number;
+    title: string;
+    author: string;
+    date: string;
+}
+
+type Props = {
+    posts: post[];
+}
+*/
+interface post  {
+    id: number;
+    title: string;
+    author: string;
+    date: string;
+}
+interface Props  {
+    posts: post[];
+}
+
 
 
 //component 
-const PostList = () => {
+const PostList: React.FC<Props> = ({ posts }) => {
+//const PostList = ({posts}: Props) => {
  
     return (
         <table className="post-list">
@@ -16,12 +43,20 @@ const PostList = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>3</td>
-                    <td>React Mockup 테스트 게시글입니다.</td>
-                    <td>홍길동</td>
-                    <td>2025-11-10</td>
-                </tr>
+                {
+                    posts.map( (post, i) => (
+
+                        <tr>
+                            <td>{i+1}</td>
+                            <td>{post.title}</td>
+                            <td>{post.author}</td>
+                            <td>{post.date}</td>
+                        </tr>
+
+                    ) )
+                }
+
+                {/*}
                 <tr>
                     <td>2</td>
                     <td>간단한 HTML 구조 확인</td>
@@ -34,6 +69,7 @@ const PostList = () => {
                     <td>이영희</td>
                     <td>2025-11-08</td>
                 </tr>
+                {*/}
             </tbody>
         </table>
     );
